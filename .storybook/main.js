4707811506f0b1781314b1fa50134f09d29e7bbe
Kai <tehhavok@gmail.com>
2020-05-16 15:54:00 -0400

    Update storybook config with new addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,19 @@ module.exports = {
   addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-actions",
-    "@storybook/addon-links"
+    "@storybook/addon-links",
+    "@storybook/addon-knobs/register",
+    {
+      name: "@storybook/addon-storysource",
+      options: {
+        loaderOptions: {
+          prettierConfig: {
+            printWidth: 80,
+            singleQuote: false,
+            trailingComma: "none"
+          }
+        }
+      }
+    }
   ]
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,16 @@
+import { addParameters } from "@storybook/react";
+import { themes } from "@storybook/theming";
 import "../src/index.scss";
+//import kaiDark from './kai-theme-dark';
+
+addParameters({
+  options: {
+    theme: themes.dark
+  }
+});
+
+/*
+addons.setConfig({
+  theme: kaiDark,
+});
+ */
